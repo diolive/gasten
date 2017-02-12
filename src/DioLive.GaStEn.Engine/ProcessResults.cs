83257ed00ea0c521fdc5bc64c3d8d08a6f56d4c8
DioLive Studio <dio@live.ru>
2017-02-12 09:28:59 +0400

    Add comments for ProcessResults enum values

--- src/DioLive.GaStEn.Engine/ProcessResults.cs
+++ src/DioLive.GaStEn.Engine/ProcessResults.cs
@@ -2,8 +2,19 @@
 {
     public enum ProcessResults
     {
+        /// <summary>
+        /// Message was successfully processed
+        /// </summary>
         Success,
+
+        /// <summary>
+        /// Message was not processed due to wrong message sequence or game state
+        /// </summary>
         NoAction,
+
+        /// <summary>
+        /// Message processing was failed
+        /// </summary>
         Failed,
     }
 }
